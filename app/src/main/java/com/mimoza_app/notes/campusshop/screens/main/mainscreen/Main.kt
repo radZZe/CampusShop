f96610aa71bb592d6d4d43cd8e72f243e3799824
antahix <antahix@yandex.ru>
2022-07-13 12:18:18 +1000

    Main RV receives data from FirestoreFirebase

--- app/src/main/java/com/mimoza_app/notes/campusshop/screens/main/mainscreen/Main.kt
+++ app/src/main/java/com/mimoza_app/notes/campusshop/screens/main/mainscreen/Main.kt
@@ -1,19 +1,33 @@
 package com.mimoza_app.notes.campusshop.screens.main
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.database.*
+import com.google.firebase.firestore.*
 import com.mimoza_app.notes.campusshop.databinding.FragmentMainBinding
+import com.mimoza_app.notes.campusshop.models.ShopItem
 import com.mimoza_app.notes.campusshop.screens.main.mainscreen.MainListAdapter
+import com.mimoza_app.notes.campusshop.util.APP_ACTIVITY
+import com.mimoza_app.notes.campusshop.util.KEY_COLLECTION_ITEMS
+import com.mimoza_app.notes.campusshop.util.showToast
 
 
 class main : Fragment() {
 
     private var _binding: FragmentMainBinding? = null
     private val mBinding get() = _binding!!
+    private lateinit var rvShopList: RecyclerView
+    private lateinit var database: FirebaseFirestore
+    private lateinit var adapter: MainListAdapter
+    private lateinit var itemsArrayList: ArrayList<ShopItem>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,19 +39,43 @@ class main : Fragment() {
 
     override fun onStart() {
         super.onStart()
-        setupRecyclerView()
-    }
-
-    private fun setupRecyclerView() {
-        val rvShopList = mBinding.rvShopList
-        with(rvShopList) {
-            val mainListAdapter = MainListAdapter()
-            adapter = mainListAdapter
+        rvShopList = mBinding.rvShopList
+        with (rvShopList) {
             recycledViewPool.setMaxRecycledViews(MainListAdapter.VIEW_TYPE, MainListAdapter.MAX_POOL_SIZE)
+            layoutManager = GridLayoutManager(APP_ACTIVITY, 2)
+            // layoutManager = LinearLayoutManager(activity)
+            setHasFixedSize(true)
         }
-            mBinding.rvShopList.layoutManager = GridLayoutManager(activity, 2)
 
+        itemsArrayList = arrayListOf()
+        adapter = MainListAdapter(itemsArrayList)
+        rvShopList.adapter = adapter
+
+        getData()
     }
 
+    private fun getData() {
+        database = FirebaseFirestore.getInstance()
+        database.collection(KEY_COLLECTION_ITEMS)
+            .addSnapshotListener(object : EventListener<QuerySnapshot>{
+
+                @SuppressLint("NotifyDataSetChanged")
+                override fun onEvent(value: QuerySnapshot?, error: FirebaseFirestoreException?) {
+
+                    if (error != null) {
+                        Log.d("Firestore Error", error.message.toString())
+                        return
+                    } else {
+                        for (dc: DocumentChange in value?.documentChanges!!) {
+                            if (dc.type == DocumentChange.Type.ADDED) {
+                                itemsArrayList.add(dc.document.toObject(ShopItem::class.java))
+                            }
+                        }
+
+                        adapter.notifyDataSetChanged()
+                    }
+                }
+            })
+    }
 
 }
